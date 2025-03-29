9d33e10c69a392559724663b384e6956e315456f
abdel hamed <vfor12@yahoo.com>
2025-03-29 03:27:43 +0200

    fix: update CORS origin to match production URL

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // غيّره ليطابق واجهتك الأمامية
+    origin: "https://ai-powered-notes.vercel.app/", // غيّره ليطابق واجهتك الأمامية
     credentials: true,
   })
 );
